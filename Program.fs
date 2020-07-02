c89009e1e9b4a23354b2a3c9acc5b59395332508
Michał <m>
2020-07-02 11:53:06 +0200

    visual studio code

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open TrafficSimulator
+open DomainFunctions
+
+[<EntryPoint>]
+let main argv =    
+
+    let composeSimpleUpdater = 
+        let progressCalc  = progressTravelledCalculator roadLenghtProvider 
+        let locationUpdater vehicle timeChange = simpleLocationUpdater (progressCalc vehicle timeChange)
+        let vehicleUpdater vehicle timeChange = vehicle |> simpleVehicleUpdater (locationUpdater vehicle timeChange)
+        vehicleUpdater
+    let updater = composeSimpleUpdater
+    
+    printfn "Hello World from F#!"
+    //printfn "%A" DomainModel.z
+    0 // return an integer exit code
